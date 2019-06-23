ccfdce882ad035c4d0579167a0c486dd1407cad5
wyu <kharma@gmail.com>
2019-06-22 22:10:48 -0700

    initial commit

---
+++ 03_clean.py
@@ -0,0 +1,22 @@
+import csv
+import pandas as pd
+import numpy as np
+import scipy
+from scipy import stats
+pd.options.mode.chained_assignment = None
+
+# Plotting packages; documentation consulted for examples:
+# Reference: https://seaborn.pydata.org/examples/index.html
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (15.0, 7.5)
+
+# Statistics packages
+import statsmodels
+from statsmodels.formula.api import ols
+
+# Image import packages
+from IPython.display import Image
+from IPython.core.display import HTML
+
+print('Script done running!')
