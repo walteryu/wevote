429bff75f877de58bcdba771ee3fac837343b59d
wyu <kharma@gmail.com>
2019-06-23 09:55:55 -0700

    add vader module

--- 03_clean.py
+++ 03_clean.py
@@ -1,19 +1,20 @@
 import csv
-import pandas as pd
-import numpy as np
-import scipy
-from scipy import stats
-pd.options.mode.chained_assignment = None
+import vaderSentiment
+# import pandas as pd
+# import numpy as np
+# import scipy
+# from scipy import stats
+# pd.options.mode.chained_assignment = None
 
 # Plotting packages; documentation consulted for examples:
 # Reference: https://seaborn.pydata.org/examples/index.html
-import seaborn as sns
-import matplotlib.pyplot as plt
-plt.rcParams['figure.figsize'] = (15.0, 7.5)
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# plt.rcParams['figure.figsize'] = (15.0, 7.5)
 
 # Statistics packages
-import statsmodels
-from statsmodels.formula.api import ols
+# import statsmodels
+# from statsmodels.formula.api import ols
 
 # Reference: NLP tutorial with Python Vader package
 # https://medium.com/analytics-vidhya/simplifying-social-media-sentiment-analysis-using-vader-in-python-f9e6ec6fc52f
@@ -21,4 +22,8 @@ from statsmodels.formula.api import ols
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyser = SentimentIntensityAnalyzer()
 
-print('Script done running!')
+def sentiment_analyzer_scores(sentence):
+    score = analyser.polarity_scores(sentence)
+    print("{:-<40} {}".format(sentence, str(score)))
+
+sentiment_analyzer_scores("The phone is super cool.")
